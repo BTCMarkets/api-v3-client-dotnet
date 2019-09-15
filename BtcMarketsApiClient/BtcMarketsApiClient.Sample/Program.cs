dc6765af0cc8e277ffdd2630bff71014ab9cf974
Nick Kvizhinadze <NickKvizhinadze@users.noreply.github.com>
2019-09-16 01:19:38 +0400

    Added orders samples (#2)
     Added orders samples (get, create, delete)

--- BtcMarketsApiClient/BtcMarketsApiClient.Sample/Program.cs
+++ BtcMarketsApiClient/BtcMarketsApiClient.Sample/Program.cs
@@ -1,17 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using BtcMarketsApiClient.Sample.Models;
+
 
 namespace BtcMarketsApiClient.Sample
 {
     class Program
     {
+        private const string BaseUrl = "https://api.btcmarkets.net";
+        private const string ApiKey = "add api key here";
+        private const string PrivateKey = "add private key here";
         static void Main(string[] args)
         {
-            var apiClient = new ApiClient();
-            apiClient.Get("/v3/orders", "status=open").Wait();
+            var orders = new Orders(BaseUrl, ApiKey, PrivateKey);
+
+            //Get Orders Sample
+
+            orders.GetOrdersAsync().Wait();
+
+
+            //Place new Order sample 
+            /*
+            var newOrder = new NewOrderModel
+            {
+                MarketId = "BTC-AUD",
+                Price = "100.12",
+                Amount = "1.034",
+                Type = "Limit",
+                Side = "Bid"
+            };
+
+            orders.PlaceNewOrder(newOrder).Wait();
+            */
+
+            //Cancel Order
+            /*
+                orders.CancelOrder("1224732").Wait();
+            */
         }
     }
 }
